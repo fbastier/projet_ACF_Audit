6c1bb05afc18c8aaf4c1eda786713c2ac1638524
CorentinPineault <p-corentin@outlook.fr>
2021-07-14 10:04:07 +0200

    Fix temporaire CommandLineRunner
    deleteAll() ajouté temporairement pour permettre au dossier d'être généré

--- src/main/java/com/acfaudit/AcfAuditApplication.java
+++ src/main/java/com/acfaudit/AcfAuditApplication.java
@@ -6,22 +6,13 @@ import com.acfaudit.user.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.annotation.Resource;
-
 
 @SpringBootApplication
 //@EnableJpaRepositories(basePackageClasses = ClientRepository.class)
-public class AcfAuditApplication implements CommandLineRunner{
-	
-	@Resource
-	StorageService storageService;
+public class AcfAuditApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(AcfAuditApplication.class, args);
@@ -29,9 +20,12 @@ public class AcfAuditApplication implements CommandLineRunner{
         System.out.println("Coucou");
     }
     
-    @Override
-    public void run(String... arg) throws Exception {
-      storageService.init();
-    }
+    @Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 
 }
